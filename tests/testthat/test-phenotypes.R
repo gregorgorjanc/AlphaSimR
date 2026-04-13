8367acb1a59cc17d4b789ecc03c97588f9af7926
Gregor Gorjanc <gregor.gorjanc@gmail.com>
2026-04-13 22:54:40 +0100

    Added SimParam

--- tests/testthat/test-phenotypes.R
+++ tests/testthat/test-phenotypes.R
@@ -113,3 +113,164 @@ test_that("pop@gv and genParam(pop)@gv match", {
   test = abs(diff) < .Machine$double.eps^0.5
   expect_true(all(test))
 })
+
+makePhenoFinalizerTestSetup = function() {
+  set.seed(101)
+  founderPop = quickHaplo(nInd = 10, nChr = 1, segSites = 4)
+  SP = SimParam$new(founderPop)
+  # SP$nThreads = 1L
+  SP$addTraitA(nQtlPerChr = 4, mean = c(0, 0), var = c(1, 2), corA = diag(2))
+  SP$setVarE(varE = c(1, 1))
+
+  list(founderPop = founderPop, SP = SP)
+}
+
+test_that("finalizePop can recode stored phenotypes", {
+  setup = makePhenoFinalizerTestSetup()
+  founderPop = setup$founderPop
+  SP = setup$SP
+
+  set.seed(201)
+  pop = newPop(founderPop, simParam = SP)
+
+  SP$finalizePop = function(pop, simParam = SP, targetTrait = 1L, ...) {
+    pop@pheno[, targetTrait] = asCategorical(x = pheno(pop)[, targetTrait])
+    pop
+  }
+
+  set.seed(201)
+  finalizedPop = newPop(founderPop, simParam = SP)
+
+  expected = pheno(pop)
+  expected[, 1] = asCategorical(x = expected[, 1])
+
+  expect_equal(pheno(finalizedPop), expected)
+
+  set.seed(201)
+  finalizedPop = newPop(founderPop, simParam = SP, targetTrait = 2L)
+
+  expected = pheno(pop)
+  expected[, 2] = asCategorical(x = expected[, 2])
+
+  expect_equal(pheno(finalizedPop), expected)
+})
+
+test_that("finalizePheno can recode phenotypes during newPop", {
+  setup = makePhenoFinalizerTestSetup()
+  founderPop = setup$founderPop
+  SP = setup$SP
+
+  set.seed(202)
+  pop = newPop(founderPop, simParam = SP)
+
+  SP$finalizePheno = function(
+    pheno,
+    pop,
+    simParam = SP,
+    targetTrait = 1L,
+    ...
+  ) {
+    pheno[, targetTrait] = asCategorical(x = pheno[, targetTrait])
+    pheno
+  }
+
+  set.seed(202)
+  finalizedPop = newPop(founderPop, simParam = SP)
+
+  expected = pheno(pop)
+  expected[, 1] = asCategorical(x = expected[, 1])
+
+  expect_equal(pheno(finalizedPop), expected)
+
+  set.seed(202)
+  finalizedPop = newPop(founderPop, simParam = SP, targetTrait = 2L)
+
+  expected = pheno(pop)
+  expected[, 2] = asCategorical(x = expected[, 2])
+
+  expect_equal(pheno(finalizedPop), expected)
+
+  set.seed(202)
+  finalizedPop = setPheno(finalizedPop, simParam = SP, targetTrait = 2L)
+
+  expected = pheno(pop)
+  expected[, 2] = asCategorical(x = expected[, 2])
+
+  expect_equal(pheno(finalizedPop), expected)
+})
+
+test_that("finalizePheno can recode stored phenotypes via setPheno", {
+  setup = makePhenoFinalizerTestSetup()
+  founderPop = setup$founderPop
+  SP = setup$SP
+  pop = newPop(founderPop, simParam = SP)
+
+  set.seed(203)
+  expected = setPheno(pop, varE = c(1, 1), onlyPheno = TRUE, simParam = SP)
+
+  SP$finalizePheno = function(
+    pheno,
+    pop,
+    simParam = SP,
+    targetTrait = 1L,
+    ...
+  ) {
+    pheno[, targetTrait] = asCategorical(x = pheno[, targetTrait])
+    pheno
+  }
+
+  set.seed(203)
+  finalizedPop = setPheno(pop, varE = c(1, 1), simParam = SP, targetTrait = 2L)
+
+  expected[, 2] = asCategorical(x = expected[, 2])
+
+  expect_equal(pheno(finalizedPop), expected)
+})
+
+test_that("finalizePheno can recode onlyPheno output from setPheno", {
+  setup = makePhenoFinalizerTestSetup()
+  founderPop = setup$founderPop
+  SP = setup$SP
+  pop = newPop(founderPop, simParam = SP)
+
+  set.seed(204)
+  pheno = setPheno(pop, varE = c(1, 1), onlyPheno = TRUE, simParam = SP)
+
+  SP$finalizePheno = function(
+    pheno,
+    pop,
+    simParam = SP,
+    targetTrait = 1L,
+    ...
+  ) {
+    pheno[, targetTrait] = asCategorical(x = pheno[, targetTrait])
+    pheno
+  }
+
+  set.seed(204)
+  finalizedPheno = setPheno(
+    pop,
+    varE = c(1, 1),
+    onlyPheno = TRUE,
+    simParam = SP
+  )
+
+  expected = pheno
+  expected[, 1] = asCategorical(x = expected[, 1])
+
+  expect_equal(finalizedPheno, expected)
+
+  set.seed(204)
+  finalizedPheno = setPheno(
+    pop,
+    varE = c(1, 1),
+    onlyPheno = TRUE,
+    simParam = SP,
+    targetTrait = 2L
+  )
+
+  expected = pheno
+  expected[, 2] = asCategorical(x = expected[, 2])
+
+  expect_equal(finalizedPheno, expected)
+})
